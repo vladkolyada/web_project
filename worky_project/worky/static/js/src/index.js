1644c044b3b3da16b807c497ef4d26f70c738501
vladkolyada <v.kolyada7.13@gmail.com>
2023-07-29 12:11:37 +0300

    added head of site

--- worky_project/worky/static/js/src/index.js
+++ worky_project/worky/static/js/src/index.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Hello(props) {
-    return (
-        <h1>Hello! {props.name}.</h1>
-    );
-}
 
-function App() {
-    return (
-        <div>
-            <Hello name="Harry" />
-            <Hello name="Ron" />
-            <Hello name="Hermione" />
-        </div>
-    );
-}
 
 
 ReactDOM.render(<App />, document.querySelector('#root'));
