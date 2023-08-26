d3a90e3c2e505be679137601b0c9c63bf8814bbb
vladkolyada <v.kolyada7.13@gmail.com>
2023-08-26 19:35:43 +0300

    added new elements, styles, even js functional for page

--- worky_project/worky/static/js/src/index.js
+++ worky_project/worky/static/js/src/index.js
@@ -1,1 +1,8 @@
+const textarea = document.getElementById('expandingTextarea');
+const stickyButton = document.getElementById('send_button');
+const endBlock = document.getElementsByClassName('end_block');
 
+textarea.addEventListener('input', () => {
+  stickyButton.style.top = textarea.scrollHeight - textarea.clientHeight + 'px';
+  endBlock.style.top = textarea.scrollHeight - textarea.clientHeight + 'px';
+});
